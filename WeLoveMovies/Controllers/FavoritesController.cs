d90fd4d3051fb214f2d21ccb5dfee1102f8542d0
Erwin <coronele@hotmail.com>
2020-03-06 14:38:16 -0500

    Friday start commit - FavoritesController added

---
+++ WeLoveMovies/Controllers/FavoritesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeLoveMovies.Models;
+using WeLoveMovies.Controllers;
+
+namespace WeLoveMovies.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private readonly WeLoveMoviesDbContext _context;
+
+        public FavoritesController(WeLoveMoviesDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult FavoriteList(string movieId)
+        {
+            var newFavorite = new Favorites();
+            newFavorite.UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            newFavorite.MovieId = movieId;
+
+            if (ModelState.IsValid)
+            {
+                _context.Favorites.Add(newFavorite);
+                _context.SaveChanges();
+
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult DeleteFavorite(int id)
+        {
+            Favorites foundFavorite = _context.Favorites.Find(id);
+            if (foundFavorite != null)
+            {
+                _context.Remove(foundFavorite);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
